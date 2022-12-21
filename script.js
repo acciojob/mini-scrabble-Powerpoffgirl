76b54b442be579a7d7805f2188ef21358225757f
Jyoti <email.jyotisingh13@gmail.com>
2022-12-21 14:01:22 +0530

    completed

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+let text = document.getElementById('evaluatedText');
+let letterCount = document.getElementById('letterCount');
+
+function count(){
+    letterCount.innerText = text.value.length+1;
+}
